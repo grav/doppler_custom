4a40d3378fd10ff512eacc90a13c3a9550f21e80
Mikkel Gravgaard <mikkel@klokke.dk>
2021-11-18 22:58:12 +0100

    wip

--- make.sh
+++ make.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run --user $(id -u):$(id -g) -it -v $PWD:/PRJ icestorm bash -c 'cd PRJ && make clean && make'
+# docker run --user $(id -u):$(id -g) -it -v $PWD:/PRJ icestorm bash -c 'cd PRJ && make clean && make'
+make
+
+( cd doppler_custom_arduino && arduino-cli upload -b "dadamachines - M4:samd:doppler" -p /dev/ttyACM0 )
