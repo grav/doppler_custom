0b3e9e590d6efb42ffdb22931abb59ea2bccc677
Mikkel Gravgaard <mikkel@klokke.dk>
2021-11-18 23:03:12 +0100

    wip

--- make.sh
+++ make.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+set -euvx
+
+## make firmware (either with docker or natively)
 # docker run --user $(id -u):$(id -g) -it -v $PWD:/PRJ icestorm bash -c 'cd PRJ && make clean && make'
 make clean; make
 
-( cd doppler_custom_arduino && arduino-cli upload -b "dadamachines - M4:samd:doppler" -p /dev/ttyACM0 )
+## Compile and upload
+## "Fully qualified board name" and port found with `arduino-cli board list`
+FQBN="dadamachines - M4:samd:doppler"
+( cd doppler_custom_arduino && arduino-cli compile --fqbn "$FQBN" && arduino-cli upload --fqbn "$FQBN" --port /dev/ttyACM0 )
