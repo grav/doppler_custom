76ef9244700125f39a4af26399285670abd4c8e1
Mikkel Gravgaard <mikkel@klokke.dk>
2021-11-19 09:10:10 +0100

    remove cruft

--- doppler_custom.v
+++ doppler_custom.v
@@ -11,112 +11,64 @@ module top ( inout  [7:0] pinbank1,             // breakout io pins F11,  F12 , 
      inout F25, F32
       );            
  
- // use ice40up5k 48Mhz internal oscillator
- wire clk; 
- //wire clk48; 
- SB_HFOSC inthosc ( .CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(clk) );
- // pll my100mhz(.clock_in(clk48), .clock_out(clk) /* , output locked */ );
+  // use ice40up5k 48Mhz internal oscillator
+  wire clk; 
+  SB_HFOSC inthosc ( .CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(clk) );
+ 
+  // configure/connect IO Pins for LED driver logic
+  SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) led_io1 ( .PACKAGE_PIN(kled[0]), .OUTPUT_ENABLE(kled_tri[0]), .D_OUT_0(1'b1)  );  // .D_IN_0(dummy2)
+  SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) led_io2 ( .PACKAGE_PIN(kled[1]), .OUTPUT_ENABLE(kled_tri[1]), .D_OUT_0(1'b1)  ); 
+  SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) led_io3 ( .PACKAGE_PIN(kled[2]), .OUTPUT_ENABLE(kled_tri[2]), .D_OUT_0(1'b1)  );
+  SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) led_io4 ( .PACKAGE_PIN(kled[3]), .OUTPUT_ENABLE(kled_tri[3]), .D_OUT_0(1'b1)  );
 
- // configure/connect IO Pins for LED driver logic
- SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) led_io1 ( .PACKAGE_PIN(kled[0]), .OUTPUT_ENABLE(kled_tri[0]), .D_OUT_0(1'b1)  );  // .D_IN_0(dummy2)
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) led_io2 ( .PACKAGE_PIN(kled[1]), .OUTPUT_ENABLE(kled_tri[1]), .D_OUT_0(1'b1)  ); 
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) led_io3 ( .PACKAGE_PIN(kled[2]), .OUTPUT_ENABLE(kled_tri[2]), .D_OUT_0(1'b1)  );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) led_io4 ( .PACKAGE_PIN(kled[3]), .OUTPUT_ENABLE(kled_tri[3]), .D_OUT_0(1'b1)  );
- 
- // reg [23:0] pullup_cfg;  // TUT NICHT weil nur Parm!
+  // Make connections from Pins in top tu Registers and support INPUT/ OUTPUT arrays
+  reg [23:0] pin_state_out ;
+  wire [23:0] pin_state_in;
+  reg  [23:0] out_eneable_cfg;
 
-
- // Make connections from Pins in top tu Registers and support INPUT/ OUTPUT arrays
- reg [23:0] pin_state_out ;
- wire [23:0] pin_state_in;
- reg  [23:0] out_eneable_cfg;
- 
- 
- assign  pin_state_out = 24'h0;
- assign  out_eneable_cfg = 24'h0;
- 
- // sorry but sometime i need to read src like a excelsheet ;-)
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_0  ( .PACKAGE_PIN(pa19),     .OUTPUT_ENABLE(out_eneable_cfg[0]),  .D_OUT_0(pin_state_out[0]) ,  .D_IN_0(pin_state_in[20]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_1  ( .PACKAGE_PIN(pa20),     .OUTPUT_ENABLE(out_eneable_cfg[1]),  .D_OUT_0(pin_state_out[1]) ,  .D_IN_0(pin_state_in[21]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_2  ( .PACKAGE_PIN(pa21),     .OUTPUT_ENABLE(out_eneable_cfg[2]),  .D_OUT_0(pin_state_out[2]) ,  .D_IN_0(pin_state_in[22]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_3  ( .PACKAGE_PIN(pa22),     .OUTPUT_ENABLE(out_eneable_cfg[3]),  .D_OUT_0(pin_state_out[3]) ,  .D_IN_0(pin_state_in[23]) );
   
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b0) ) upin_4  ( .PACKAGE_PIN(pinbank1[0]),  .OUTPUT_ENABLE(out_eneable_cfg[4]),  .D_OUT_0(pin_state_out[4]) ,  .D_IN_0(pin_state_in[4]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b0) ) upin_5  ( .PACKAGE_PIN(pinbank1[1]),  .OUTPUT_ENABLE(out_eneable_cfg[5]),  .D_OUT_0(pin_state_out[5]) ,  .D_IN_0(pin_state_in[5]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_6  ( .PACKAGE_PIN(pinbank1[2]),  .OUTPUT_ENABLE(out_eneable_cfg[6]),  .D_OUT_0(pin_state_out[6]) ,  .D_IN_0(pin_state_in[6]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_7  ( .PACKAGE_PIN(pinbank1[3]),  .OUTPUT_ENABLE(out_eneable_cfg[7]),  .D_OUT_0(pin_state_out[7]) ,  .D_IN_0(pin_state_in[7]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_8  ( .PACKAGE_PIN(pinbank1[4]),  .OUTPUT_ENABLE(out_eneable_cfg[8]),  .D_OUT_0(pin_state_out[8]) ,  .D_IN_0(pin_state_in[8]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_9  ( .PACKAGE_PIN(pinbank1[5]),  .OUTPUT_ENABLE(out_eneable_cfg[9]),  .D_OUT_0(pin_state_out[9]) ,  .D_IN_0(pin_state_in[9]) ); 
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_10  ( .PACKAGE_PIN(pinbank1[6]),  .OUTPUT_ENABLE(out_eneable_cfg[10]), .D_OUT_0(pin_state_out[10]) , .D_IN_0(pin_state_in[10]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_11  ( .PACKAGE_PIN(pinbank1[7]),  .OUTPUT_ENABLE(out_eneable_cfg[11]), .D_OUT_0(pin_state_out[11]) , .D_IN_0(pin_state_in[11]) );
-
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_12 ( .PACKAGE_PIN(pinbank2[0]),  .OUTPUT_ENABLE(out_eneable_cfg[12]), .D_OUT_0(pin_state_out[12]) , .D_IN_0(pin_state_in[12]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_13 ( .PACKAGE_PIN(pinbank2[1]),  .OUTPUT_ENABLE(out_eneable_cfg[13]),  .D_OUT_0(pin_state_out[13]) , .D_IN_0(pin_state_in[13]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_14 ( .PACKAGE_PIN(pinbank2[2]),  .OUTPUT_ENABLE(out_eneable_cfg[14]),  .D_OUT_0(pin_state_out[14]) , .D_IN_0(pin_state_in[14]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_15  ( .PACKAGE_PIN(pinbank2[3]),  .OUTPUT_ENABLE(out_eneable_cfg[15]),  .D_OUT_0(pin_state_out[15]) , .D_IN_0(pin_state_in[15]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_16  ( .PACKAGE_PIN(pinbank2[4]),  .OUTPUT_ENABLE(out_eneable_cfg[16]),  .D_OUT_0(pin_state_out[16]) , .D_IN_0(pin_state_in[16]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_17  ( .PACKAGE_PIN(pinbank2[5]),  .OUTPUT_ENABLE(out_eneable_cfg[17]),  .D_OUT_0(pin_state_out[17]) , .D_IN_0(pin_state_in[17]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_18  ( .PACKAGE_PIN(pinbank2[6]),  .OUTPUT_ENABLE(out_eneable_cfg[18]), .D_OUT_0(pin_state_out[18]) , .D_IN_0(pin_state_in[18]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_19  ( .PACKAGE_PIN(pinbank2[7]),  .OUTPUT_ENABLE(out_eneable_cfg[19]), .D_OUT_0(pin_state_out[19]) , .D_IN_0(pin_state_in[19]) );
+  // see this https://youtu.be/IOmG5y7VMrg?t=49m32s (german only)
+  // Cross Domain Clock Syncing! SPI_INCOMING_CLK
+  reg spi_clk1,spi_clk2;
+  wire spi_clk_negedge = ( ~spi_clk1 &&  spi_clk2)  ;
+  wire spi_clk_posedge = (  spi_clk1 && ~spi_clk2)  ;
+  always @(posedge clk) begin
+   spi_clk1 <= cfg_sck;
+   spi_clk2 <= spi_clk1;
+  end
  
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_20  ( .PACKAGE_PIN(button1),   .OUTPUT_ENABLE(1'b0),        .D_OUT_0(pin_state_out[20]) , .D_IN_0(pin_state_in[0]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_21  ( .PACKAGE_PIN(button2),   .OUTPUT_ENABLE(1'b0),        .D_OUT_0(pin_state_out[21]) , .D_IN_0(pin_state_in[1]) ); 
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_22  ( .PACKAGE_PIN(F25),     .OUTPUT_ENABLE(out_eneable_cfg[22]),  .D_OUT_0(pin_state_out[22]) , .D_IN_0(pin_state_in[2]) );
-    SB_IO #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b1) ) upin_23  ( .PACKAGE_PIN(F32),     .OUTPUT_ENABLE(out_eneable_cfg[23]),  .D_OUT_0(pin_state_out[23]) , .D_IN_0(pin_state_in[3]) );
-
-
- 
- // see this https://youtu.be/IOmG5y7VMrg?t=49m32s (german only)
- // Cross Domain Clock Syncing! SPI_INCOMING_CLK
- reg spi_clk1,spi_clk2;
- wire spi_clk_negedge = ( ~spi_clk1 &&  spi_clk2)  ;
- wire spi_clk_posedge = (  spi_clk1 && ~spi_clk2)  ;
- always @(posedge clk) begin
-  spi_clk1 <= cfg_sck;
-  spi_clk2 <= spi_clk1;
- end
-
- // Cross Domain Clock Syncing! SPI_INCOMING_CS + register Set
- reg spi_cs1,spi_cs2;
- wire spi_cs_negedge = ( ~spi_cs1 &&  spi_cs2)  ;
- wire spi_cs_posedge = (  spi_cs1 && ~spi_cs2)  ;
- always @(posedge clk) begin
-  spi_cs1 <= cfg_cs;
-  spi_cs2 <= spi_cs1;
- end /*
- reg cs;
- always @(posedge clk) begin
-  if(spi_cs_posedge)    cs<= 1'b1;
-  else if(spi_cs_negedge)  cs<= 1'b0;
- end
- */
- 
- // Cross Domain Clock Syncing! SPI_INCOMING_MOSI + register Set
- reg spi_mosi1,spi_mosi2;
- wire spi_mosi_negedge = ( ~spi_mosi1 &&  spi_mosi2)  ;
- wire spi_mosi_posedge = (  spi_mosi1 && ~spi_mosi2)  ;
- always @(posedge clk) begin
-  spi_mosi1 <= cfg_si;
-  spi_mosi2 <= spi_mosi1;
- end
- reg mosi;
- always @(posedge clk) begin
-  if(spi_mosi_posedge)    mosi<= 1'b1;
-  else if(spi_mosi_negedge)  mosi<= 1'b0;
- end
-
- // Spi Shifter
- reg [15:0]  spi_in;
- reg [15:0]  miso_shift;
+  // Cross Domain Clock Syncing! SPI_INCOMING_CS + register Set
+  reg spi_cs1,spi_cs2;
+  wire spi_cs_negedge = ( ~spi_cs1 &&  spi_cs2)  ;
+  wire spi_cs_posedge = (  spi_cs1 && ~spi_cs2)  ;
+  always @(posedge clk) begin
+   spi_cs1 <= cfg_cs;
+   spi_cs2 <= spi_cs1;
+  end 
   
-
+  // Cross Domain Clock Syncing! SPI_INCOMING_MOSI + register Set
+  reg spi_mosi1,spi_mosi2;
+  wire spi_mosi_negedge = ( ~spi_mosi1 &&  spi_mosi2)  ;
+  wire spi_mosi_posedge = (  spi_mosi1 && ~spi_mosi2)  ;
+  always @(posedge clk) begin
+   spi_mosi1 <= cfg_si;
+   spi_mosi2 <= spi_mosi1;
+  end
+  reg mosi;
+  always @(posedge clk) begin
+   if(spi_mosi_posedge)    mosi<= 1'b1;
+   else if(spi_mosi_negedge)  mosi<= 1'b0;
+  end
  
-
- // Led
- wire [3:0]  kled_tri;   // connect katode via SB_IO modules to allow high impadance  or 3.3V
- reg [15:0] data16 ;   // data register for 16 leds
+  // Spi Shifter
+  reg [15:0]  spi_in;
+  reg [15:0]  miso_shift;
   
- wire [9:0] saw_out;
+  // Led
+  wire [3:0]  kled_tri;   // connect katode via SB_IO modules to allow high impadance  or 3.3V
+  reg [15:0] data16 ;   // data register for 16 leds
+   
+  wire [9:0] saw_out;
   wire [9:0] sine_out;
   wire [9:0] pdm_sine_err;
   wire [9:0] pdm_saw_err;
@@ -127,24 +79,20 @@ module top ( inout  [7:0] pinbank1,             // breakout io pins F11,  F12 , 
   pdm p1(.clk(clk),.din(saw_out),.rst(button1),.dout(LED1),.error(pdm_saw_err));    
   LED16 myleds (.clk(clk), .ledbits(data16) ,  .aled(aled), .kled_tri(kled_tri) );
 
-//   // sine_gen#(.CLKSPEED(100_000),.FREQ(2)) s2(.clk(clk),.out(sine_out));
-//   // pdm p2(.clk(clk),.din(sine_out),.rst(button1),.dout(LED2),.error(pdm_sine_err));    
- assign cfg_so = miso_shift[15];
- always @(posedge clk) begin
-  //  data16 <= LED1 ? 23 : 0; // doesn't work for some reason
-  if(spi_cs_posedge) begin
-   data16    <= spi_in;       // Just Write data 2 LED
-  end else if(spi_cs_negedge) begin
-   // miso_shift  <= data16;      // loopbackTest
-   miso_shift  <= pin_state_in[15:0]; // PinRead
-   //miso_shift  <= 16'h53f0;     // constValues answer just for tesing
-  end else begin
-   if(spi_clk_posedge)  spi_in[15:0]    <= {spi_in[14:0] ,    mosi};
-   if(spi_clk_posedge)  miso_shift[15:0]  <= {miso_shift[14:0] ,  1'b1};
-  end
-
-
- end
+  assign cfg_so = miso_shift[15];
+  always @(posedge clk) begin
+    //  data16 <= LED1 ? 23 : 0; // doesn't work for some reason
+    if(spi_cs_posedge) begin
+     data16    <= spi_in;       // Just Write data 2 LED
+    end else if(spi_cs_negedge) begin
+     // miso_shift  <= data16;      // loopbackTest
+     miso_shift  <= pin_state_in[15:0]; // PinRead
+     //miso_shift  <= 16'h53f0;     // constValues answer just for tesing
+    end else begin
+     if(spi_clk_posedge)  spi_in[15:0]    <= {spi_in[14:0] ,    mosi};
+     if(spi_clk_posedge)  miso_shift[15:0]  <= {miso_shift[14:0] ,  1'b1};
+    end
+   end
   
 endmodule  // end top module
 
