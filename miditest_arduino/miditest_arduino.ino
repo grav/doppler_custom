f48864d49ee7bea393fdf822607e93bb8bd92809
Mikkel Gravgaard <mikkel@klokke.dk>
2021-11-24 23:36:46 +0100

    wip

--- miditest_arduino/miditest_arduino.ino
+++ miditest_arduino/miditest_arduino.ino
@@ -1,16 +1,29 @@
 #include <ICEClass.h>
-// #include "MIDIUSB.h"
+#include "MIDIUSB.h"
 #include "miditest.h" // set up the path to doppler_simple_io or custom firmware
 ICEClass ice40;   // subclass ICEClass to implement custom spi protocol
  
 void setup() {
    ice40.upload(miditest_bin,sizeof(miditest_bin)); // Upload BitStream Firmware to FPGA -> see variant.h
    ice40.initSPI();  // start SPI runtime Link to FPGA
+    ice40.sendSPI16(0);   
+
+  // midi stuff
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
 }
 
 void loop() {
-     
-  static uint16_t x = 0;
-  ice40.sendSPI16(x++);   
-  delay(10);
+  //MidiUSB.accept();
+  //delayMicroseconds(1);
+  midiEventPacket_t rx;
+  do {
+    rx = MidiUSB.read();
+    if (rx.header == 0x09) {
+      ice40.sendSPI16(1);   
+    } else if ( rx.header == 0x08){
+      ice40.sendSPI16(0);
+    }
+  } while (rx.header != 0);
 }
