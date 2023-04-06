e9ae1d2adc4231da3b0b1663cf6fe0edf2079e96
Mikkel Gravgaard <mikkel@klokke.dk>
2023-04-06 12:04:38 +0200

    WIP

--- doppler_synth.v
+++ doppler_synth.v
@@ -35,28 +35,38 @@ module top (
 
   localparam clockspeed = 50_000_000;
 
-  // douple clock speed to get lower than 1 freq (0.5Hz)
-  saw #(.CLKSPEED(clockspeed*2),.FREQ(1)) s1(.clk(clk),.out(saw_out));
-  // putting eg `button1` as `.rst` param produces weird results,
-  // so disabling reset by putting constant 0
-  pdm p1(.clk(clk),.din(saw_out),.rst(0),.dout(LED1),.error(pdm_saw_err));    
+//   // douple clock speed to get lower than 1 freq (0.5Hz)
+//   saw #(.CLKSPEED(clockspeed*2),.FREQ(1)) s1(.clk(clk),.out(saw_out));
+//   // putting eg `button1` as `.rst` param produces weird results,
+//   // so disabling reset by putting constant 0
+//   pdm p1(.clk(clk),.din(saw_out),.rst(0),.dout(LED1),.error(pdm_saw_err));    
   
   
   
-  sine_gen#(.CLKSPEED(clockspeed), .FREQ(4), .MAX_FREQ_MOD(1024) ) 
-  s2(
-    .clk(clk),
-    .freq_mod(saw_out),
-    .out(sine_out)
-    );
+//   sine_gen#(.CLKSPEED(clockspeed), .FREQ(4), .MAX_FREQ_MOD(1024) ) 
+//   s2(
+//     .clk(clk),
+//     .freq_mod(saw_out),
+//     .out(sine_out)
+//     );
   
-  pdm p2(.clk(clk),.din(sine_out),.rst(0),.dout(LED2),.error(pdm_sine_err)); 
+//   pdm p2(.clk(clk),.din(sine_out),.rst(0),.dout(LED2),.error(pdm_sine_err)); 
   
   LED16 myleds (.clk(clk), .ledbits(data16), .aled(aled), .kled_tri(kled_tri));
 
+  // putting eg `button1` as `.gate` param produces weird results,
+  // so disabling reset by putting constant 0
+
+    Synth #(.CLKSPEED(clockspeed), .SINE_FREQ(1000)) 
+    s(
+        .clk(clk),
+        .gate(0),
+        .amp_in(1023),
+        .dout(F32), 
+        .aux_out1(F25));
+
   always @(posedge clk) begin
     data16 <= (LED1 ? 32 : 0) + (LED2 ? 1024 : 0); 
-    
   end
   
 endmodule  // end top module
--- synth.v
+++ synth.v
@@ -4,6 +4,8 @@ module Synth (input clk, input gate, input [15:0] amp_in, output dout, output au
   // when initiating Synth module
   parameter CLKSPEED = 50_000_000;// clockspeed of Nexys A7
   parameter SINE_FREQ = 440;
+  parameter SAW_FREQ = 1;
+
 
   wire [9:0] saw_out;
   wire [9:0] sine_out;
@@ -13,7 +15,7 @@ module Synth (input clk, input gate, input [15:0] amp_in, output dout, output au
 
 
   // douple clock speed to get lower than 1 freq (0.5Hz)
-  saw #(.CLKSPEED(CLKSPEED*2),.FREQ(1)) s1(.clk(clk),.rst(gate),.out(saw_out));
+  saw #(.CLKSPEED(CLKSPEED*2),.FREQ(SAW_FREQ)) s1(.clk(clk),.rst(gate),.out(saw_out));
   // putting eg `button1` as `.rst` param produces weird results,
   // so disabling reset by putting constant 0
   pdm p1(.clk(clk),.din(saw_out),.rst(0),.dout(aux_out1),.error(pdm_saw_err));    
