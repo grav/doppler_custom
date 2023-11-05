0922bdc95012213da51275c372b3597fb1dc0f11
Mikkel Gravgaard <mikkel@klokke.dk>
2023-11-05 11:09:07 +0100

    WIP

--- blink/blink.v
+++ blink/blink.v
@@ -6,8 +6,6 @@ module top (
   // Gotcha: removing aled here stops blinking from working, eventhough aled doesn't seem to be in use(!)
   output [3:0] kled,
   output [3:0] aled,
-  // defined in doppler.pcf
-  output F25,
 
 );            
  
@@ -21,19 +19,15 @@ module top (
   // configure/connect IO Pins for LED driver logic
   SB_IO 
     #( .PIN_TYPE(6'b 1010_01), .PULLUP(1'b 0) ) 
-    led_io1 ( .PACKAGE_PIN(kled[0]), .OUTPUT_ENABLE(kled_tri), .D_OUT_0(1'b1)  );  // .D_IN_0(dummy2)
+    led_io1 ( .PACKAGE_PIN(kled[0]), .OUTPUT_ENABLE(kled_tri), .D_OUT_0(1'b1)  ); 
   
 
-  reg led_on; 
   reg [22:0] counter; 
 
   always @(posedge clk) begin
     counter<=counter+1 ; 
     if (counter == 0) begin
-      led_on = !led_on;
-      kled_tri <= led_on ? 1 : 0;
-      // Assign a voltage to pin F25
-      F25 <= kled_tri > 0 ? 1 : 0;
+      kled_tri = ~kled_tri;
     end
 
   end
