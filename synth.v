04694a60737015cb91f32ffb97b41b827c44b110
Mikkel Gravgaard <mikkel@klokke.dk>
2024-10-10 22:35:34 +0200

    WIP

--- synth.v
+++ synth.v
@@ -7,7 +7,7 @@ module Synth (input clk, input gate, input [15:0] amp_in, output dout, output au
   parameter SAW_FREQ = 1;
 
 
-  wire [9:0] saw_out;
+  wire [9:0] modulator;
   wire [9:0] sine_out;
   wire [9:0] pdm_sine_err;
   wire [9:0] pdm_saw_err;
@@ -15,17 +15,17 @@ module Synth (input clk, input gate, input [15:0] amp_in, output dout, output au
 
 
   // douple clock speed to get lower than 1 freq (0.5Hz)
-  saw #(.CLKSPEED(CLKSPEED*2),.FREQ(SAW_FREQ)) s1(.clk(clk),.rst(gate),.out(saw_out));
+  saw #(.CLKSPEED(CLKSPEED*2),.FREQ(SAW_FREQ)) s1(.clk(clk),.rst(gate),.out(modulator));
   // putting eg `button1` as `.rst` param produces weird results,
   // so disabling reset by putting constant 0
-  pdm p1(.clk(clk),.din(saw_out),.rst(0),.dout(aux_out1),.error(pdm_saw_err));    
+  pdm p1(.clk(clk),.din(modulator),.rst(0),.dout(aux_out1),.error(pdm_saw_err));    
   
   
   
   sine_gen#(.CLKSPEED(CLKSPEED), .FREQ(SINE_FREQ), .MAX_FREQ_MOD(1024) ) 
   s2(
     .clk(clk),
-    .freq_mod(saw_out),
+    .freq_mod(modulator),
     .out(sine_out)
     );
 
