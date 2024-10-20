9dd93a4165e4ef14d1ecb8788a0b2a514693057e
Mikkel Gravgaard <mikkel@klokke.dk>
2024-10-20 21:06:20 +0200

    WIP

--- make.sh
+++ make.sh
@@ -7,9 +7,19 @@ set -euvx pipefail
 # docker run --user $(id -u):$(id -g) -it -v $PWD:/PRJ icestorm bash -c 'cd PRJ && make clean && make'
 make clean PROJ="$PROJ"; make PROJ="$PROJ"
 
+
 ## Compile and upload
 ## "Fully qualified board name" and port found with `arduino-cli board list`
 FQBN="dadamachines - M4:samd:doppler"
+
+
 port=$(arduino-cli board list | grep "$FQBN" |  awk '{print $1}' )
-( cd "${PROJ}_arduino" && arduino-cli compile --fqbn "$FQBN" && arduino-cli upload --fqbn "$FQBN" --port "$port" )
+pushd "${PROJ}_arduino" 
+arduino-cli compile --fqbn "$FQBN" 
+if [ -z "$SKIP_UPLOAD" ]; then
+    arduino-cli upload --fqbn "$FQBN" --port "$port" 
+else
+    echo >&2 "*** Skipping upload"
+fi
+popd
 
